b1ce822fbdb0536c3290b4c0c3620cffdc754ebd
Yash Ratnani <86655022+yashratnani02@users.noreply.github.com>
2021-07-12 14:05:28 +0530

    Update and rename sssp(trees)_bfs.cpp to sssp_bfs_dfs.cpp

--- sssp_bfs_dfs.cpp
+++ sssp_bfs_dfs.cpp
@@ -13,3 +13,18 @@ queue<int> que;
 
 //This Calculate Shortest Path of All Nodes to Source
 //SSSP with DFS works only on trees
+
+
+
+void dfs(int node, int d){
+    vis[node] = 1;
+    dist[node] = d;
+    for(auto child : adj[node]){
+        if(!vis[child]){
+            dfs(child,dist[node] + 1);
+        }
+    }
+}
+
+
+    dfs(1,0);
