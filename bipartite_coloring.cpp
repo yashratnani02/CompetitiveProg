a9ee16be1b2e76921a40cd0d21f62785ae1b30df
Yash Ratnani <86655022+yashratnani02@users.noreply.github.com>
2021-07-12 14:23:33 +0530

    Update bipartite_coloring.cpp

--- bipartite_coloring.cpp
+++ bipartite_coloring.cpp
@@ -1,1 +1,13 @@
+int color[n] = {0};
 
+void dfs(int node, int col){
+    vis[node] = 1;
+    color[node] = col;
+    for(auto child : adj[node]){
+        if(!vis[child]){
+            dfs(child,1^col);
+        }
+    }
+}
+
+    dfs(src,0);
